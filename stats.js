97b3f6cec303cc83d46619a5c7a74a56cc58decd
Tyler Carr <tylercarr1111@yahoo.com>
2017-12-06 14:32:28 -0600

    Edited how data is extracted and added map name and champion name to page

--- stats.js
+++ stats.js
@@ -8,53 +8,59 @@ function storeStats(participants){
     var playerSix = {}
     for(let i=0;i<participants.length;i++){
       if(i==0){
-        playerOne.score = participants[i].score;
-        playerOne.damageDone = participants[i].damageDone;
-        playerOne.disablesDone = participants[i].disablesDone;
-        playerOne.healingDone = participants[i].healingDone;
-        playerOne.kills = participants[i].kills;
-        playerOne.deaths = participants[i].deaths;
+        playerOne.score = participants[i].stats.score;
+        playerOne.damageDone = participants[i].stats.damageDone;
+        playerOne.disablesDone = participants[i].stats.disablesDone;
+        playerOne.healingDone = participants[i].stats.healingDone;
+        playerOne.kills = participants[i].stats.kills;
+        playerOne.deaths = participants[i].stats.deaths;
+        playerOne.champion = getChampionByID(participants[i].actor);
       }
       if(i==1){
-        playerTwo.score = participants[i].score;
-        playerTwo.damageDone = participants[i].damageDone;
-        playerTwo.disablesDone = participants[i].disablesDone;
-        playerTwo.healingDone = participants[i].healingDone;
-        playerTwo.kills = participants[i].kills;
-        playerTwo.deaths = participants[i].deaths;
+        playerTwo.score = participants[i].stats.score;
+        playerTwo.damageDone = participants[i].stats.damageDone;
+        playerTwo.disablesDone = participants[i].stats.disablesDone;
+        playerTwo.healingDone = participants[i].stats.healingDone;
+        playerTwo.kills = participants[i].stats.kills;
+        playerTwo.deaths = participants[i].stats.deaths;
+        playerTwo.champion = getChampionByID(participants[i].actor);
       }
       if(i==2){
-        playerThree.score = participants[i].score;
-        playerThree.damageDone = participants[i].damageDone;
-        playerThree.disablesDone = participants[i].disablesDone;
-        playerThree.healingDone = participants[i].healingDone;
-        playerThree.kills = participants[i].kills;
-        playerThree.deaths = participants[i].deaths;
+        playerThree.score = participants[i].stats.score;
+        playerThree.damageDone = participants[i].stats.damageDone;
+        playerThree.disablesDone = participants[i].stats.disablesDone;
+        playerThree.healingDone = participants[i].stats.healingDone;
+        playerThree.kills = participants[i].stats.kills;
+        playerThree.deaths = participants[i].stats.deaths;
+        playerThree.champion = getChampionByID(participants[i].actor);
       }
       if(i==3){
-        playerfour.score = participants[i].score;
-        playerfour.damageDone = participants[i].damageDone;
-        playerfour.disablesDone = participants[i].disablesDone;
-        playerfour.healingDone = participants[i].healingDone;
-        playerfour.kills = participants[i].kills;
-        playerfour.deaths = participants[i].deaths;
+        playerfour.score = participants[i].stats.score;
+        playerfour.damageDone = participants[i].stats.damageDone;
+        playerfour.disablesDone = participants[i].stats.disablesDone;
+        playerfour.healingDone = participants[i].stats.healingDone;
+        playerfour.kills = participants[i].stats.kills;
+        playerfour.deaths = participants[i].stats.deaths;
+        playerfour.champion = getChampionByID(participants[i].actor);
       }
       if(participants.length>4){
         if(i==4){
-          playerFive.score = participants[i].score;
-          playerFive.damageDone = participants[i].damageDone;
-          playerFive.disablesDone = participants[i].disablesDone;
-          playerFive.healingDone = participants[i].healingDone;
-          playerFive.kills = participants[i].kills;
-          playerFive.deaths = participants[i].deaths;
+          playerFive.score = participants[i].stats.score;
+          playerFive.damageDone = participants[i].stats.damageDone;
+          playerFive.disablesDone = participants[i].stats.disablesDone;
+          playerFive.healingDone = participants[i].stats.healingDone;
+          playerFive.kills = participants[i].stats.kills;
+          playerFive.deaths = participants[i].stats.deaths;
+          playerFive.champion = getChampionByID(participants[i].actor);
         }
         if(i==5){
-          playerSix.score = participants[i].score;
-          playerSix.damageDone = participants[i].damageDone;
-          playerSix.disablesDone = participants[i].disablesDone;
-          playerSix.healingDone = participants[i].healingDone;
-          playerSix.kills = participants[i].kills;
-          playerSix.deaths = participants[i].deaths;
+          playerSix.score = participants[i].stats.score;
+          playerSix.damageDone = participants[i].stats.damageDone;
+          playerSix.disablesDone = participants[i].stats.disablesDone;
+          playerSix.healingDone = participants[i].stats.healingDone;
+          playerSix.kills = participants[i].stats.kills;
+          playerSix.deaths = participants[i].stats.deaths;
+          playerSix.champion = getChampionByID(participants[i].actor);
         }
       }
     }
@@ -76,25 +82,29 @@ function getStats(){
     document.getElementById("playerOneStats").innerHTML += "Damage Done: " + playersArray[0].damageDone + "<br />";
     document.getElementById("playerOneStats").innerHTML += "Disables Done: " + playersArray[0].disablesDone + "<br />";
     document.getElementById("playerOneStats").innerHTML += "Healing Done: " + playersArray[0].healingDone + "<br />";
-    document.getElementById("playerOneStats").innerHTML += "Kills: " + playersArray[0].Kills + "<br />";
+    document.getElementById("playerOneStats").innerHTML += "Kills: " + playersArray[0].kills + "<br />";
     document.getElementById("playerOneStats").innerHTML += "Deaths: " + playersArray[0].deaths + "<br />";
+    document.getElementById("playerOneStats").innerHTML += "Deaths: " + playersArray[0].champion + "<br />";
     document.getElementById("playerTwoStats").innerHTML += "Score: " + playersArray[1].score + "<br />";
     document.getElementById("playerTwoStats").innerHTML += "Damage Done: " + playersArray[1].damageDone + "<br />";
     document.getElementById("playerTwoStats").innerHTML += "Disables Done: " + playersArray[1].disablesDone + "<br />";
     document.getElementById("playerTwoStats").innerHTML += "Healing Done: " + playersArray[1].healingDone + "<br />";
-    document.getElementById("playerTwoStats").innerHTML += "Kills: " + playersArray[1].Kills + "<br />";
+    document.getElementById("playerTwoStats").innerHTML += "Kills: " + playersArray[1].kills + "<br />";
     document.getElementById("playerTwoStats").innerHTML += "Deaths: " + playersArray[1].deaths + "<br />";
+    document.getElementById("playerTwoStats").innerHTML += "Deaths: " + playersArray[1].champion + "<br />";
     document.getElementById("PlayerThreeStats").innerHTML += "Score: " + playersArray[2].score + "<br />";
     document.getElementById("PlayerThreeStats").innerHTML += "Damage Done: " + playersArray[2].damageDone + "<br />";
     document.getElementById("PlayerThreeStats").innerHTML += "Disables Done: " + playersArray[2].disablesDone + "<br />";
     document.getElementById("PlayerThreeStats").innerHTML += "Healing Done: " + playersArray[2].healingDone + "<br />";
-    document.getElementById("PlayerThreeStats").innerHTML += "Kills: " + playersArray[2].Kills + "<br />";
+    document.getElementById("PlayerThreeStats").innerHTML += "Kills: " + playersArray[2].kills + "<br />";
     document.getElementById("PlayerThreeStats").innerHTML += "Deaths: " + playersArray[2].deaths + "<br />";
+    document.getElementById("PlayerThreeStats").innerHTML += "Deaths: " + playersArray[2].champion + "<br />";
     document.getElementById("playerFourStats").innerHTML += "Score: " + playersArray[3].score + "<br />";
     document.getElementById("playerFourStats").innerHTML += "Damage Done: " + playersArray[3].damageDone + "<br />";
     document.getElementById("playerFourStats").innerHTML += "Disables Done: " + playersArray[3].disablesDone + "<br />";
     document.getElementById("playerFourStats").innerHTML += "Healing Done: " + playersArray[3].healingDone + "<br />";
-    document.getElementById("playerFourStats").innerHTML += "Kills: " + playersArray[3].Kills + "<br />";
+    document.getElementById("playerFourStats").innerHTML += "Kills: " + playersArray[3].kills + "<br />";
     document.getElementById("playerFourStats").innerHTML += "Deaths: " + playersArray[3].deaths + "<br />";
-
+    document.getElementById("playerFourStats").innerHTML += "Deaths: " + playersArray[3].champion + "<br />";
+    document.getElementById("mapName").innerHTML = "Map: " + sessionStorage.mapName;
 }
